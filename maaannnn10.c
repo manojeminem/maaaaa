c580c8c42d13c7780959172ee5f07202f892b550
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-01-30 16:58:57 +0530

    Create maaannnn10.c

---
+++ maaannnn10.c
@@ -0,0 +1,25 @@
+ 
+ #include<stdio.h>
+ void main()
+ {
+ int a,b,c, result = 0;
+
+    printf("Enter a three digit integer: ");
+    scanf("%d", &a);
+
+    b= a;
+
+    while (b!= 0)
+    {
+        c = b%10;
+        result += c*c*c;
+        b /= 10;
+    }
+
+    if(result == a)
+        printf("%d is an Armstrong number.",a);
+    else
+        printf("%d is not an Armstrong number.",a);
+
+    return 0;
+    }

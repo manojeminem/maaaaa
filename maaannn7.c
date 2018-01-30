5aad5e12583696b788fc51fafdca65f428d563d9
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-01-30 16:52:36 +0530

    Create maaannn7.c

---
+++ maaannn7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a,b,c;
+clrscr();
+printf("\n the value of :");
+scanf("%d%d",&a,&b);
+c=(a/b==1);
+for(i=0;i<b,i++)
+if (i%==0)
+{
+printf("\n the value is%d,i);
+}
+else
+{
+printf("\n the value is %d,c);
+}
+getch();
+}
